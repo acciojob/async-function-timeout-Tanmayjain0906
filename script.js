c62dabfc702b47e0498b1957c9a312dc9f3f8787
Tanmay jain <134151751+Tanmayjain0906@users.noreply.github.com>
2023-08-25 20:31:58 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,4 +1,19 @@
-//your JS code here. If required.
-let text = document.getElementById("text");
 
-let delay = document.getElementById("delay");
+let text;
+let delay;
+
+let button = document.getElementById("btn");
+let div = document.getElementById("output");
+
+button.addEventListener("click", () => {
+
+	text = document.getElementById("text").value;
+	delay = document.getElementById("delay").value;
+
+	async function getData()
+		{
+			await new Promise(resolve => setTimeout(resolve, delay*1000));
+            div.innerText = text;	
+		}
+	getData();
+})
