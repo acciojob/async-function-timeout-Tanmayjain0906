458fff1594b9342e9c41063e014f0a0f1e4f90d5
Tanmay jain <134151751+Tanmayjain0906@users.noreply.github.com>
2023-08-25 20:39:23 +0530

    created a submission

--- script.js
+++ script.js
@@ -3,6 +3,7 @@ let text;
 let delay;
 
 let button = document.getElementById("btn");
+
 let div = document.getElementById("output");
 
 button.addEventListener("click", () => {
@@ -10,6 +11,11 @@ button.addEventListener("click", () => {
 	text = document.getElementById("text").value;
 	delay = document.getElementById("delay").value;
 
+	console.log(text=="");
+	 if (text == "" || delay == "") {
+       div.innerText = 'Please Enter Some Value in Input';
+       return;
+     }
 	async function getData()
 		{
 			await new Promise(resolve => setTimeout(resolve, delay*1000));
